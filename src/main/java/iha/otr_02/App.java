86f75c6dda8fb1e0394f5862af99862ef92de803
Ildar Akhmetov <akhmetov.ih@yandex.ru>
2017-01-26 15:27:36 +0300

    First code commit

---
+++ src/main/java/iha/otr_02/App.java
@@ -0,0 +1,48 @@
+package iha.otr_02;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class App 
+{
+    public static HashMap<String, ArrayList<String>> notebook = 
+    		new HashMap<String ,ArrayList<String>>();
+	
+    public static ArrayList<String> phoneList = null;
+    
+	public static void init(){
+        phoneList = new ArrayList<String>();
+        phoneList.add("+8 800 2000 500");
+        phoneList.add("+8 800 200 600");
+        notebook.put("Иванов И.И.", phoneList);
+ 	
+        phoneList = new ArrayList<String>();
+        phoneList.add("+8 800 2000 700");
+        notebook.put("Петров П.П.", phoneList);
+     	
+        phoneList = new ArrayList<String>();
+        phoneList.add("+8 800 2000 800");
+        phoneList.add("+8 800 2000 900");
+        phoneList.add("+8 800 2000 000");
+        notebook.put("Сидоров С.С.", phoneList);
+	}
+	
+	public static void main( String[] args )
+    {
+		init();
+        
+		phoneList = new ArrayList<String>();
+		
+		if (args.length == 1) {
+			phoneList = notebook.get(args[0]);
+			if (phoneList.size() != 0) {
+				System.out.println("User " + args[0] + " contain next phone numbers:");
+				for (String item : phoneList) System.out.println(item);
+			} else {
+				System.out.println( args[0] + " have not a phone numbers!" );
+			}
+		} else {
+			System.out.println("Not enter correct parameters!");
+		}
+    }
+}
---
+++ src/test/java/iha/otr_02/AppTest.java
@@ -0,0 +1,17 @@
+package iha.otr_02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Test class for Hello world!
+ *
+ */
+
+public class AppTest {
+    @Test
+    public void testIsThisReallyTrue() {
+        assertTrue(true);
+    }
+}
